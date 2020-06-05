1a44f920eaa2ecfd258a3780fd7baedc5e334d91
OleksiiKolotenko <60075219+OleksiiKolotenko@users.noreply.github.com>
2020-06-05 23:19:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,223 +1,276 @@
 let move = 0;
-let undoArr = [];
-let redoArr = [];
-let cells = document.querySelectorAll('.cell');
-
-if(localStorage.getItem('move')){
-    move = localStorage.getItem('move');
+for (let i of document.querySelectorAll(".cell")) {
+    i.onclick = function() {
+        click(i);
+    };
 }
 
-if(localStorage.getItem('undoArr')){
-    undoArr = JSON.parse(localStorage.getItem('undoArr'));
-}
-
-if(localStorage.getItem('redoArr')){
-    redoArr = JSON.parse(localStorage.getItem('redoArr'));
-}
-
-if(localStorage.getItem('undoArr')){
-    for(let q = 0; q < undoArr.length; q++){
-        document.getElementById(undoArr[q].id).classList.add(undoArr[q].type);
-    }
-}
-
-if(undoArr.length > 0){
-    document.querySelector('.undo-btn').disabled = false;
-}
-
-if(redoArr.length > 0){
-    document.querySelector('.redo-btn').disabled = false;
-}
-
-document.querySelector('.field').onclick = function ticTacToe(event){
-    if(document.querySelector('.hidden')){
-        if(event.target.className == 'cell'){
-            if(move % 2 == 0){
-                event.target.classList.add('ch');
-                undoArr.push(new step(event.target.id,'ch'));
-                redoArr = [];
-                document.querySelector('.redo-btn').disabled = true;
-            }
-            else{
-                event.target.classList.add('r');
-                undoArr.push(new step(event.target.id,'r'));
-                redoArr = [];
-                document.querySelector('.redo-btn').disabled = true;
-            }
+// adding X and O's
+document.querySelector('.field').onclick = function(event){
+    if(event.target.className == 'cell') {
+        if(move % 2 == 0){
+            event.target.classList.add('ch');
         }
-        if(undoArr.length > 0){
+        else{
+            event.target.classList.add('r');
+let step = 0;
+let move = true;
+let story = [];
+let count = 1;
+
+if (localStorage.getItem("data")){  recover(); }
+
+function click(el) {
+    if (move) {
+        if (!el.hasChildNodes()) {
+            if (count != 1){
+                for (let i = 1; i < count; i++) {
+                    story.pop();
+                }
+                count = 1;
+            }
+              if (step % 2 === 0) {
+                let X = document.createElement('div');
+                X.className = 'ch';
+                el.appendChild(X);
+            }
+              else {
+                let O = document.createElement('div');
+                O.className = 'r';
+                el.appendChild(O);
+            }
+              step++;
+            story.push(el.id);
             document.querySelector('.undo-btn').disabled = false;
+            document.querySelector('.redo-btn').disabled = true;
+            Check();
         }
-        checkWin();
         move++;
     }
-    localStorage.setItem('move', move);
-    localStorage.setItem('undoArr', JSON.stringify(undoArr));
-    localStorage.setItem('redoArr', JSON.stringify(redoArr));
 }
 
-function step(id, type) {
-    this.id = id;
-    this.type = type;
+  document.querySelector('.undo-btn').onclick = function() {
+    step--;
+  document.getElementById(story[story.length - count])
+    .removeChild(document.getElementById(story[story.length - count]).firstChild);
+document.querySelector('.redo-btn').disabled = false;
+if (story.length - count === 0) {
+    document.querySelector('.undo-btn').disabled = true;
 }
+count++;
+};
 
-document.querySelector('.undo-btn').addEventListener('click', function(){
-    let undoObj = undoArr.pop();
-    redoArr.push(undoObj);
-    document.querySelector('.redo-btn').disabled = false;
-    document.getElementById(undoObj.id).classList.remove(undoObj.type);
-    move--;
-    if(undoArr.length == 0){
-        document.querySelector('.undo-btn').disabled = true;
-    }
-    localStorage.setItem('move', move);
-    localStorage.setItem('undoArr', JSON.stringify(undoArr));
-    localStorage.setItem('redoArr', JSON.stringify(redoArr));
-});
-
-document.querySelector('.redo-btn').addEventListener('click', function(){
-    let redoObj = redoArr.pop();
-    undoArr.push(redoObj);
-    document.getElementById(redoObj.id).classList.add(redoObj.type);
+    if(move > 0) {
+        document.querySelector('.undo-btn').disabled = false;
+document.querySelector('.redo-btn').onclick = function() {
     document.querySelector('.undo-btn').disabled = false;
-    checkWin();
-    move++;
-    if(redoArr.length == 0){
+    count--;
+    if (story.length - count > story.length - 2) {
         document.querySelector('.redo-btn').disabled = true;
     }
-    localStorage.setItem('move', move);
-    localStorage.setItem('undoArr', JSON.stringify(undoArr));
-    localStorage.setItem('redoArr', JSON.stringify(redoArr));
-});
+    if(move <= 0) {
+        document.querySelector('.undo-btn').disabled = true;
+    if (step % 2 === 0) {
+        let X = document.createElement('div');
+        X.className = 'ch';
+        document.getElementById(story[story.length - count]).appendChild(X);
+    } else {
+        let O = document.createElement('div');
+        O.className = 'r';
+        document.getElementById(story[story.length - count]).appendChild(O);
+    }
+    stepp++;
+};
 
-
-let size = Math.sqrt(document.querySelectorAll('.cell').length);
-let arr = new Array(), rows, cols;
-let c = 0;
-for (rows = 0; rows < size; rows++) {
-    arr[rows] = new Array();
-    for (cols = 0; cols < size; cols++) {
-        arr[rows][cols] = c;
-        c++;
-    }
-}
-
-
-function checkWin(){
-    let player = (move % 2 == 0) ? 'ch' : 'r';
-    let winCells = [];
-    //vertical
-    for (rows = 0; rows < size; rows++) {
-        matches = 0;
-        for (cols = 0; cols < size; cols++) {
-            if (document.getElementById(`c-${arr[cols][rows]}`).classList.contains(player)) {
-                matches++;
-                winCells.push(arr[cols][rows]);
-                if (matches >= size) {
-                    win(1, winCells);
-                }
-            } 
-            else {
-                matches = 0;
-                winCells = [];
-            }
-        }
-    }
-    //horizontal
-    for (rows = 0; rows < size; rows++) {
-        matches = 0;
-        for (cols = 0; cols < size; cols++) {
-            if (document.getElementById(`c-${arr[rows][cols]}`).classList.contains(player)) {
-                matches++;
-                winCells.push(arr[rows][cols]);
-                if (matches >= size) {
-                    win(2, winCells);
-                }
-            } 
-            else {
-                matches = 0;
-                winCells = [];
-            }
-        }
-    }
-    //diagonal-right
-    matches = 0;
-    winCells = [];
-    for (rows = 0; rows < size; rows++) {
-        if(document.getElementById(`c-${arr[rows][rows]}`).classList.contains(player)){
-            winCells.push(arr[rows][rows]);
-            matches++;
-            if (matches >= size) {
-                win(3, winCells);
-            }
-        } 
-        else {
-            winCells = [];
-            matches = 0;
-        }
-    }
-    //diagonal-left
-    matches = 0;
-    winCells = [];
-    for (rows = size-1; rows >= 0; rows--) {
-        if(document.getElementById(`c-${arr[rows][Math.abs((size - 1) - rows)]}`).classList.contains(player)){
-            winCells.push(arr[rows][Math.abs((size - 1) - rows)]);
-            matches++;
-            if (matches >= size) {
-                win(4, winCells);
-            }
-        } 
-        else {
-            winCells = [];
-            matches = 0;
-        }
-    }
-    if(undoArr.length >= cells.length && document.querySelector('.hidden')){
-        draw();  
-    }
-}
-
-function win(number, winCells){
-    document.querySelector('.won-title').classList.remove('hidden');
-    if(move % 2 == 0){
-        document.querySelector('.won-message').innerHTML = 'Crosses won!';
-    }
-    else{
-        document.querySelector('.won-message').innerHTML = 'Toes won!';
-    }
-
-    switch(number){
-        case 1:
-            winCells.forEach(function(e){document.getElementById(`c-${e}`).classList.add('win', 'vertical')});
-        break;
-        case 2:
-            winCells.forEach(function(e){document.getElementById(`c-${e}`).classList.add('win', 'horizontal')});
-        break;
-        case 3:
-            winCells.forEach(function(e){document.getElementById(`c-${e}`).classList.add('win', 'diagonal-right')});
-        break;
-        case 4:
-            winCells.forEach(function(e){document.getElementById(`c-${e}`).classList.add('win', 'diagonal-left')});
-        break;
-    }
-    document.querySelector('.undo-btn').disabled = true;
-    document.querySelector('.redo-btn').disabled = true;
-    undoArr = [];
-}
-function draw(){
-    document.querySelector('.won-title').classList.remove('hidden');
-    document.querySelector('.won-message').innerHTML = 'It is a draw!';
-    document.querySelector('.undo-btn').disabled = true;
-    document.querySelector('.redo-btn').disabled = true;
-    undoArr = [];
-}
-document.querySelector('.restart-btn').onclick = function reset(){
-    for (x = 0; x < cells.length; x++) {
-        cells[x].classList.remove('ch', 'r', 'win', 'vertical', 'horizontal', 'diagonal-right', 'diagonal-left');
-    }
+// undo redo
+document.querySelector('.undo').onclick = addEventListener("click", function e(event)){
+    move--;
+    function e() {
+document.querySelector('.restart.btn').onclick = function() {
+    step = 0;
+    story = [];
+    move = true;
     document.querySelector('.won-title').classList.add('hidden');
-    move = 0;
-    undoArr = [];
-    document.querySelector('.undo-btn').disabled = true;
-    localStorage.clear();
+    RemoveMarkedCells();
+};
+
+function Check() {
+    let size = document.querySelector('.field').childNodes.length;
+    let MarkedCells = 0;
+    for (let i = 0; i < size; i++) {
+        for (let e = 0; e < size; e++) {
+            if (!document.querySelector('.field').childNodes[i].childNodes[e].firstChild) {
+                break;
+            }
+            MarkedCells++;
+            if (e === size - 1) {
+                let firstElement = document.querySelector('.field').childNodes[i].firstChild.firstChild.className;
+
+                for (let el = 1; el < document.querySelector('.field').childNodes[i].childNodes.length; el++) {
+                    if (firstElement !== document.querySelector('.field').childNodes[i].childNodes[el].firstChild.className) {
+                        break;
+                    }
+                    if (el === size - 1) {
+                        document.querySelector('.won-title').classList.remove('hidden');
+                        if (document.querySelector('.field').childNodes[i].childNodes[el].firstChild.className === 'ch') {
+                            document.querySelector('.won-message').textContent = "Crosses won!";
+                        } else {
+                            document.querySelector('.won-message').textContent = "Toes won!";
+                        }
+                        for (let winCell = 0; winCell < size; winCell++) {
+                            document.querySelector('.field').childNodes[i].childNodes[winCell].classList.add('.win');
+                            document.querySelector('.field').childNodes[i].childNodes[winCell].classList.add('horizontal');
+                        }
+                        document.querySelector('.undo-btn').disabled =true;
+                        move = false;
+                    }
+                }
+            }
+        }
+        for (let e = 0; e < size; e++) {
+            if (!document.querySelector(".field").childNodes[e].childNodes[i].firstChild){
+                break;
+            }
+            if(e=== size - 1) {
+                let firstElement = document.querySelector(".field").firstChild.childNodes[i].firstChild.className;
+
+                for (let el =1; el < size; el++) {
+                    if (firstElement !== document.querySelector('.field').childNodes[el].childNodes[i].firstChild.className) {
+                        break;
+                    }
+                    if (el === size - 1) {
+                        document.querySelector(".won-title").classList.remove('hidden');
+                        if (document.querySelector('.field').childNodes[el].childNodes[i].firstChild.className === 'ch') {
+                            document.querySelector('.won-message').textContent = 'Crosses won!';
+                        } else {
+                            document.querySelector('.won-message').textContent = "Toes won!"
+                        }
+                        for (let winCell = 0; winCell < size; winCell++) {
+                            document.querySelector('.field').childNodes[winCell].childNodes[i].classList.add("win");
+                            document.querySelector('.field').childNodes[winCell].childNodes[i].classList.add('vertical');
+                        }
+                        document.querySelector('.undo-btn').disabled = true;
+                        move = false;
+                    }
+                }
+            }
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        if (!document.querySelector('.field').childNodes[i].firstChild) {
+            break;
+        }
+        if (i === size - 1) {
+            let firstElement = document.querySelector('.field').firstChild.firstChild.firstChild.className;
+            for (let el = 1; el < size; el++) {
+                if (firstElement !== document.querySelector('.field').childNodes[el].childNodes[el].firstChild.className) {
+                    break;
+                }
+                if (el === size - 1) {
+                    document.querySelector('.won-title').classList.remove('hidden');
+                    if (document.querySelector('.field').childNodes[el].childNodes[el].firstChild.className === 'ch') {
+                        document.querySelector('.won-message').textContent = 'Crosses won!';
+                    } else {
+                        document.querySelector('.won-message').textContent = 'Toes won!';
+                    }
+                    for (let winCell = 0; winCell < size; winCell++) {
+                        document.querySelector('.field').childNodes[winCell].childNodes[winCell].classList.add('win');
+                        document.querySelector('.field').childNodes[winCell].childNodes[winCell].classList.add('diagonal-right');
+                    }
+                    document.querySelector('.undo-btn').disabled = true;
+                    move = false;
+                }
+            }
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        if (!document.querySelector('.field').childNodes[i].childNodes[size - 1 - i].firstChild) {
+            break;
+        }
+        if (i === size - 1) {
+            let firstElement = document.querySelector('.field').firstChild.lastChild.firstChild.className;
+            for (let el = 1; el < size; el++ ) {
+                if (
+                    firstElement !==
+                    document.querySelector('.field').childNodes[el].childNodes[size -1 - el].firstChild.className
+                ) {
+                    break;
+                }
+                if (el === size - 1) {
+                    document.querySelector('.won-title').classList.remove('hidden');
+                    if (document.querySelector('.field').childNodes[el].childNodes[size - 1 - el].firstChild.className === 'ch') {
+                        document.querySelector('won-message').textContent = 'Crosses won!';
+                    } else {
+                        document.querySelector('won-message').textContent = 'Toes won!';
+                    }
+                    for (let winCell = 0; winCell < size; winCell++) {
+                        document.querySelector('.field').childNodes[winCell].childNodes[size - 1 - winCell].classList.add('win');
+                        document
+                            .querySelector('.field')
+                            .childNodes[winCell].childNodes[size - 1 - winCell].classList.add('diagonal-left');
+                    }
+                    document.querySelector('.field')
+                    move = false;
+                }
+            }
+        }
+    }
+    if (document.querySelector('won-title').classList.contains('hidden')) {
+        if (size * size === MarkedCells) {
+            document.querySelector('.won-title').classList.remove('hidden');
+            document.querySelector('.won-message').textContent = 'Draw';
+            document.querySelector('.undo-btn').disabled = true;
+        }
+    }
+}
+
+function RemoveMarkedCells() {
+    let size = document.querySelector('.field').childNodes.length;
+    for (let row = 0; row < size; row++) {
+        for (let column = 0; column < size; column++) {
+            if (document.querySelector('field').childNodes[row].childNodes[column].firstChild) {
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.remove();
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.toggle('win', false);
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.toggle('horizontal', false);
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.toggle('vertical', false);
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.toggle('diagonal-right', false);
+                document.querySelector('field').childNodes[row].childNodes[column].firstChild.toggle('diagonal-left', false);
+            }
+        }
+    }
+}
+
+function recover() {
+    let size =  document.querySelector('.field').childNodes.length;
+    let information = JSON.parse(this.localStorage.getItem('data'));
+    step = information.Step;
+    count = information.count;
+    story = information.story;
+    move = information.move;
+    if (information.UndoButton !== document.querySelector('undo-btn').disabled) {
+        document.querySelector('.undo-btn').disabled = !document.querySelector('.undo-btn').disabled;
+    }
+    if (information.RedoButton !== document.querySelector('.redo-btn').disabled) {
+        document.querySelector('redo-btn').disabled = !document.querySelector('.redo-btn').disabled;
+    }
+    if (information.State !== document.querySelector('.won-title').classList.contains('hidden')) {
+        document.querySelector('.won-title').classList.toggle('hidden');
+    }
+    document.querySelector('.won-message').textContent = information.WinMessage;
+    for (let row = 0; row < size; row++) {
+        for (let column = 0; column < size; column++ ){
+            if (information.cells[row][column] === 0) {
+                if (document.querySelector('.field'),childNodes[row].childNodes[column].firstChild) {
+                    RemoveMarkedCells();
+                } 
+            } else {
+                if (!document.querySelector('.field').childNodes[row].childNodes[column].firstChild) {
+                    let cell = document.createElement('div');
+                    cell.className = information.cells[row][column];
+                    document.querySelector('.field').childNodes[row].childNodes[column].appendChild(cell);
+                }
+            }
+        }
+    }
+    Check();
 }
